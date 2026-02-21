8858bd0493889acf039e599740e029c5821f76e6
timouteb <timmylbradford@gmail.com>
2026-02-21 18:56:29 -0500

    Update test.py
    Change the statistics to operate off of a single list instead of multiple variables.

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 from random import random
 import time
 
-rounds = 0
-besttime = 1000
-totaltime = 0
+times: list[float] = []
 
-print('\nPress Enter when the prompt appears. The prompt will appear every 0-10 seconds. Type exit to exit.\n')
+print('\nPress Enter when the prompt appears. The prompt will take up to ten seconds to appear. Type exit to exit.\n')
 input('Press Enter to begin: ')
 
 for i in range(1,101):
@@ -16,18 +14,15 @@ for i in range(1,101):
     if input("Press Enter or type exit and press enter to exit: ").upper() == 'EXIT':
         break
     timetaken = time.perf_counter()-start
-    if timetaken < besttime:
-        besttime = timetaken
-    rounds += 1
-    totaltime += timetaken
-    print(f'Your time was {timetaken:.2f}')
+    times.append(timetaken)
+    print(f'Your time was {timetaken:.2f}') 
     if i == 100:
         break
 
 print('\n---------------------------------------------------------------------------------------------------------------------------------------------\n')
-if rounds == 100:
+if len(times) == 100:
     print('You played 100 rounds and beat the game. CONGRATULATIONS. Now go do something else.\n\n')
-if rounds == 0:
+if len(times) == 0:
     print('You played zero rounds.\n\n')
 else:
-    print(f'You played {rounds} rounds. Your best time was {besttime:.2f} seconds. Your average response time was {totaltime / rounds:.2f}\n\n')
+    print(f'You played {len(times)} rounds. Your best time was {min(times):.2f} seconds. Your average response time was {sum(times) / len(times):.2f}\n\n')
